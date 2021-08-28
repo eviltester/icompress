3f93c1256ca692c4f8a7b38790b29d22265c8942
alan <alan@compendiumdev.co.uk>
2021-08-28 13:23:35 +0100

    adding todos and formatting, filled some state reporting gaps

--- bin/index.js
+++ bin/index.js
@@ -33,6 +33,9 @@ const imagesToLeaveAlone = []; // images which we need to compress
 const compressedImages = [];
 const errorProcessingImages = [];
 
+// todo: we may not need queues, we may be able to rely on the state field in the image object put this behind an abstraction and test
+// e.g. it could be queues.findFirstImageWithState(state) which knows which queue to look in for states, then when that works we could remove queues and have a central store and see if it makes a difference, if it didn't then queues would be renamed to images
+// todo: add the moveFromQToQ would be on the Queues object, (eventually this might be controlled by setting the state)
 
 const findFirstImageWithState = (desiredState, queue)=>{
     for(image of queue){
@@ -46,6 +49,9 @@ const findFirstImageWithState = (desiredState, queue)=>{
 
 function ImageStatesEnum(){
 
+    this.FETCHING_HEADERS = "FETCHING_HEADERS";
+    this.FETCHED_HEADERS = "FETCHED_HEADERS";
+    this.SHOULD_DOWNLOAD_EVALUATION = "SHOULD_DOWNLOAD_EVALUATION";
     this.AWAITING_DOWNLOAD = "AWAITING_DOWNLOAD";
     this.ABOUT_TO_DOWNLOAD = "ABOUT_TO_DOWNLOAD";
     this.IGNORED = "IGNORED";
@@ -58,6 +64,7 @@ function ImageStatesEnum(){
     this.COMPRESSING_VIA_IMAGEMAGICK = "COMPRESSING_VIA_IMAGEMAGICK";
     this.COMPRESSED_VIA_IMAGEMAGICK = "COMPRESSED_VIA_IMAGEMAGICK";
 
+    this.ERROR_FETCHING_HEADERS = "ERROR_FETCHING_HEADERS";
     this.ERROR_DOWNLOADING = "ERROR_DOWNLOADING";
     this.ERROR_FFMPEG_COMPRESS = "ERROR_FFMPEG_COMPRESS";
     this.ERROR_IMAGEMAGICK_COMPRESS = "ERROR_IMAGEMAGICK_COMPRESS";
@@ -65,6 +72,7 @@ function ImageStatesEnum(){
 
 const ImageStates = new ImageStatesEnum();
 
+// todo: setImageState and addImageErrorReport should be on an Image class
 const setImageState = (image, newState)=>{
 
     if(image.states==undefined){
@@ -78,6 +86,11 @@ const setImageState = (image, newState)=>{
     image.states.push(newState);
 }
 
+// named 'add' instead of 'set' to allow scope for expanding to multiple error reports in the future e.g. we may have some optional states
+const addImageErrorReport = (image, error)=> {
+    console.log(error);
+    image.errorReport = error;
+};
 
 
 
@@ -138,8 +151,6 @@ function execParas(commandLineTemplate, params) {
 
 
 // TODO: start making image an object/class
-// TODO: maintain an list of historical states in the image as well as a current state
-
 /*
 
 
@@ -164,29 +175,28 @@ function getImageHeaders(url) {
 
     return new Promise((resolve, reject) => {
 
+        setImageState(img,ImageStates.FETCHING_HEADERS);
+
         fetch(url, {method: 'HEAD'}).
         then(function(response) {
+            setImageState(img,ImageStates.FETCHED_HEADERS);
+            console.log(response.headers);
             img.contentLength = response.headers.get('content-length');
             img.type = response.headers.get('Content-Type');
-            console.log(response.headers);
-            console.log(img);
-            console.log(response.headers.get('content-length'));
-            console.log(response.headers.get('Content-Type'));
             resolve(img);
         }).
         catch((error)=>{
-                        img.errorReport = error;
-                        console.error('stderr:', error.stderr);
-                        reject(error);
-                    });
+            setImageState(img,ImageStates.ERROR_FETCHING_HEADERS);
+            addImageErrorReport(img, error);
+            reject(error);
+        });
 
     });
   }
 
 
   function createDir(dir){
-    console.log("creating: " +dir);
-      console.log(dir)
+    console.log("creating: "  + dir);
     try {
         if (!FS.existsSync(dir)) {
             FS.mkdirSync(dir);
@@ -206,12 +216,10 @@ function getImageHeaders(url) {
     img.fileName = fileName;
 
     setImageState(img, ImageStates.DOWNLOADING)
-    console.log(img);
 
     console.log("creating dir " +dir);
+    // todo: possibly have a state/queue for create imageDir or store output dir in the image as a field?
     createDir(dir);
-
-
 
     return new Promise((resolve, reject) => {
         const downloadTo = dir + "/" + fileName;
@@ -226,10 +234,10 @@ function getImageHeaders(url) {
             resolve(img)
         }).
         catch((error)=>{
-            setImageState(img, ImageStates.ERROR_DOWNLOADING)
-                        console.error('stderr:', error.stderr);
-                        reject(error);
-                    });
+            setImageState(img, ImageStates.ERROR_DOWNLOADING);
+            console.error('stderr:', error.stderr);
+            reject(error);
+        });
 
     });
 
@@ -299,6 +307,9 @@ function filterImagesAndAddToDownloadQueue(processQ, downloadQ, ignoreQ, maxK){
     const forRemoval= [];
 
     for(image of processQ){
+
+        setImageState(image, ImageStates.SHOULD_DOWNLOAD_EVALUATION);
+
         if(image.contentLength>=(maxK*1000)){
             setImageState(image, ImageStates.AWAITING_DOWNLOAD);
             forRemoval.push(image);
@@ -333,6 +344,7 @@ let nothingToDoCount=0;
 const quitWhenNothingToDoInterval = setInterval(()=>{
     let shouldIQuit = false;
 
+    // todo: queues.areEmpty()
     if(imagesToProcess.length===0 && imagesToCompress.length===0 && imagesToDownload.length===0 && downloadingImages.length===0 && compressingImages.length===0){
         shouldIQuit= true;
         nothingToDoCount++;
@@ -343,20 +355,23 @@ const quitWhenNothingToDoInterval = setInterval(()=>{
     console.log("nothing to do " + nothingToDoCount);
 
     // add a delay before quiting
-    if(nothingToDoCount<10){
+    // todo: add a config param for TimesToCheckQueueBeforeExiting
+    if(nothingToDoCount<5){
         reportOnQueues();
         return;
     }
 
     if(shouldIQuit){
+        // todo: queues.reportOnQueueCounts()
         reportOnQueues();
 
+        // todo: queues.reportOnQueuesContents()
         console.log("Compressed Images");
         console.log(compressedImages);
         console.log("Ignored Images");  // todo add an ignore reason to the image
         console.log(imagesToLeaveAlone);
         console.log("Error Processing Images")
-        console.log(errorProcessingImages); // todo add an error reason to the image
+        console.log(errorProcessingImages);
 
         process.exit(0); // OK I Quit
     }
@@ -367,7 +382,7 @@ const quitWhenNothingToDoInterval = setInterval(()=>{
 const downloadImagesQInterval = setInterval(()=>{
 
     const imageToDownload = findFirstImageWithState(ImageStates.AWAITING_DOWNLOAD, imagesToDownload);
-    if(imageToDownload==null){
+    if(imageToDownload==null){ // nothing in the Queue waiting to be downloaded
         return;
     }
     setImageState(imageToDownload, ImageStates.ABOUT_TO_DOWNLOAD);
@@ -377,82 +392,59 @@ const downloadImagesQInterval = setInterval(()=>{
         console.log(imagesToDownload);
         downloadFile(imageToDownload).
         then(()=>{
-            console.log("downloaded ");
-            console.log(imageToDownload);
-
             setImageState(imageToDownload, ImageStates.READY_TO_COMPRESS);
-
             moveFromQToQ(imageToDownload, downloadingImages, imagesToCompress);
-
-        }).catch((error)=>
-            {
+        }).
+        catch((error)=> {
             setImageState(imageToDownload, ImageStates.ERROR_DOWNLOADING);
-             console.log(error);
-             imageToDownload.errorReport = error;
-             moveFromQToQ(imageToDownload, downloadingImages, errorProcessingImages);
-            });
+            addImageErrorReport(imageToDownload, error)
+            moveFromQToQ(imageToDownload, downloadingImages, errorProcessingImages);
+        });
     //}
 },500)
 
-const ffmpeg = 'ffmpeg -i ${inputFileName} -lavfi "mpdecimate,fps=3,scale=0:-1:flags=lanczos[x];[x]split[x1][x2];[x1]palettegen[p];[x2][p]paletteuse" -vsync 0 -y ${outputFileName}';
+
 
 function ffmpegCompress(imageToFFmpeg, inputFileName, outputFileName) {
+
+    const ffmpeg = 'ffmpeg -i ${inputFileName} -lavfi "mpdecimate,fps=3,scale=0:-1:flags=lanczos[x];[x]split[x1][x2];[x1]palettegen[p];[x2][p]paletteuse" -vsync 0 -y ${outputFileName}';
 
     setImageState(imageToFFmpeg, ImageStates.COMPRESSING_VIA_FFMPEG);
     return new Promise((resolve, reject)=>{
         execParas(ffmpeg, {inputFileName: inputFileName, outputFileName: outputFileName})
             .then((result)=>{
                 setImageState(imageToFFmpeg, ImageStates.COMPRESSED_VIA_FFMPEG);
-                console.log(imageToFFmpeg);
                 imageToFFmpeg.ffmpeg = {inputFile: inputFileName, outputFile: outputFileName};
                 resolve(imageToFFmpeg)
             }).catch((error)=> {
                 setImageState(imageToFFmpeg, ImageStates.ERROR_FFMPEG_COMPRESS);
-                imageToFFmpeg.errorReport = error;
+                addImageErrorReport(imageToFFmpeg, error)
                 reject(imageToFFmpeg);
         })
     });
 }
 
-// thought I needed timing sync between file writes, but actually
-// I needed to add const in front of variables to make them scoped
-// rather than creating globals which get overwritten by other parts of the code!
-function wait(milliseconds) {
-    console.log("Waiting...")
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
-}
-
-const imagemagick = 'magick convert ${inputFileName} +dither -colors 32 -depth 8 ${outputFileName}';
-
+// todo: in the future allow custom commands to be added for images
 function imageMagickCompress(imageToCompress, inputFileName, outputFileName) {
+
+    // todo: allow configuration and profiles for image magick
+    const imagemagick = 'magick convert ${inputFileName} +dither -colors 32 -depth 8 ${outputFileName}';
+
     setImageState(imageToCompress, ImageStates.COMPRESSING_VIA_IMAGEMAGICK);
     return new Promise((resolve, reject)=>{
         execParas(imagemagick, {inputFileName: inputFileName, outputFileName: outputFileName})
             .then((result)=>{
                 setImageState(imageToCompress, ImageStates.COMPRESSED_VIA_IMAGEMAGICK);
-                console.log(imageToCompress);
                 imageToCompress.imagemagick = {inputFile: inputFileName, outputFile: outputFileName};
                 resolve(imageToCompress);
             }).catch((error)=> {
                 setImageState(imageToCompress, ImageStates.ERROR_IMAGEMAGICK_COMPRESS);
-                imageToCompress.errorReport = error;
+                addImageErrorReport(imageToCompress, error)
                 reject(imageToCompress);
         })
     });
 }
 
-/*
-    async is basically a promise where the resolve is the return value and the reject is the exception
- */
-// const moveFromQToQ = async (imageToMove, fromQ, toQ)=>{
-//     try {
-//         toQ.push(imageToMove);
-//         fromQ.splice(fromQ.indexOf(imageToMove), 1);
-//     }catch(error){
-//         console.log("failure to move " + error);
-//         throw imageToMove;
-//     }
-// };
 const moveFromQToQ = (imageToMove, fromQ, toQ)=>{
     toQ.push(imageToMove);
     fromQ.splice(fromQ.indexOf(imageToMove), 1);
@@ -465,8 +457,6 @@ const processCompressImagesQ = ()=>{
         return;
     }
     setImageState(imageToCompress, ImageStates.ABOUT_TO_COMPRESS);
-    console.log(imageToCompress);
-
     moveFromQToQ(imageToCompress, imagesToCompress, compressingImages);
 
     const writtenImagePath = Path.parse(imageToCompress.fullFilePath);
